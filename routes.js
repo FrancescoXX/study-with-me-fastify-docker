8483cd6dccd89f48b1e8e164b14f8c3958bec325
FrancescoXX <francescociullaroma@gmail.com>
2021-04-22 11:24:02 +0200

    postgres db image added

--- routes.js
+++ routes.js
@@ -1,5 +1,4 @@
 async function routes(fastify, options) {
-  
   // Declare a route
   fastify.get('/', async (request, reply) => {
     return { hello: 'world' };
@@ -9,6 +8,7 @@ async function routes(fastify, options) {
     return { hello: 'Srikar' };
   });
 
+  // READ ALL
   fastify.route({
     method: 'GET',
     url: '/testget',
@@ -31,6 +31,37 @@ async function routes(fastify, options) {
     },
   });
 
+  // READ ONE
+  fastify.route({
+    method: 'GET',
+    url: '/testgetone/:id',
+    schema: {
+      querystring: {
+        name: { type: 'string' },
+        excitement: { type: 'integer' },
+      },
+      params: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+        },
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            hello: { type: 'string' },
+          },
+        },
+      },
+    },
+    handler: function (request, reply) {
+      console.log(request.params);
+      reply.send('test get ' + request.params.id);
+    },
+  });
+
+  // CREATE
   fastify.route({
     method: 'POST',
     url: '/testpost',
@@ -51,6 +82,54 @@ async function routes(fastify, options) {
     handler: function (request, reply) {
       console.log(request.body);
       reply.send({ hello: 'testpost' });
+    },
+  });
+
+  // UPDATE
+  fastify.route({
+    method: 'PUT',
+    url: '/testput',
+    schema: {
+      querystring: {
+        name: { type: 'string' },
+        excitement: { type: 'integer' },
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            hello: { type: 'string' },
+          },
+        },
+      },
+    },
+    handler: function (request, reply) {
+      console.log(request.body);
+      reply.send('PUT Request');
+    },
+  });
+
+  // UPDATE
+  fastify.route({
+    method: 'DELETE',
+    url: '/testdelete',
+    schema: {
+      querystring: {
+        name: { type: 'string' },
+        excitement: { type: 'integer' },
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            hello: { type: 'string' },
+          },
+        },
+      },
+    },
+    handler: function (request, reply) {
+      console.log(request.body);
+      reply.send('DELETE Request');
     },
   });
 }
