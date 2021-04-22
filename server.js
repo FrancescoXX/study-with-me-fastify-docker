9f117e6ce8e7aef15bd887a962cb2af15f9267ec
FrancescoXX <francescociullaroma@gmail.com>
2021-04-22 12:41:56 +0200

    adsded read all, post user

--- server.js
+++ server.js
@@ -23,14 +23,13 @@ fastify.register(require('fastify-postgres'), {
 //   // await client.end();
 // }
 
-// test
-fastify.get('/user/:id', (req, reply) => {
+// init db
+fastify.get('/initDB', (req, reply) => {
   fastify.pg.connect(onConnect);
 
   function onConnect(err, client, release) {
     if (err) return reply.send(err);
 
-    // client.query('SELECT id, username, hash, salt FROM users WHERE id=$1', [req.params.id], function onResult(err, result) {
     client.query(
       'CREATE TABLE IF NOT EXISTS "users" ("id" SERIAL PRIMARY KEY,"name" varchar(30),"description" varchar(30),"tweets" integer);',
       function onResult(err, result) {
@@ -41,7 +40,76 @@ fastify.get('/user/:id', (req, reply) => {
   }
 });
 
-//GET al users
+//GET all users
+fastify.route({
+  method: 'GET',
+  url: '/users',
+  schema: {
+    querystring: {
+      name: { type: 'string' },
+      excitement: { type: 'integer' },
+    },
+    // response: {
+    //   200: {
+    //     type: 'object',
+    //   },
+    // },
+  },
+  handler: async function (request, reply) {
+    fastify.pg.connect(onConnect);
+
+    function onConnect(err, client, release) {
+      if (err) return reply.send(err);
+
+      // client.query('SELECT id, username, hash, salt FROM users WHERE id=$1', [req.params.id], function onResult(err, result) {
+      client.query('SELECT * from users', function onResult(err, result) {
+        console.log(result.rows);
+        release();
+        reply.send(err || result.rows);
+      });
+    }
+  },
+});
+
+//Create users
+fastify.route({
+  method: 'POST',
+  url: '/users',
+  schema: {
+    querystring: {
+      name: { type: 'string' },
+      excitement: { type: 'integer' },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: { type: 'string' },
+        },
+      },
+    },
+  },
+  handler: function (request, reply) {
+    fastify.pg.connect(onConnect);
+
+    function onConnect(err, client, release) {
+      if (err) return reply.send(err);
+
+      const newUser = request.body;
+      console.log('adding', newUser);
+      // client.query('SELECT id, username, hash, salt FROM users WHERE id=$1', [req.params.id], function onResult(err, result) {
+      client.query(
+        `INSERT into users (id,name,description,tweets) VALUES(${newUser.id},'${newUser.name}','${newUser.description}',${newUser.tweets})`,
+        function onResult(err, result) {
+          release();
+          reply.send(err || result);
+        }
+      );
+    }
+
+    // reply.send();
+  },
+});
 
 // Run the server!
 const start = () => {
